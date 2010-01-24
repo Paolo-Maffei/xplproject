e7070fb3cee0415ffe8e6dde846e6beac5513684
tieske <tieske@f80ddf92-7da3-a8d7-59b9-a95cd208da60>
2010-01-24 08:51:43 +0000

    first released version

---
+++ tieske.xPL_installation_aid/tags/version_1_0/Demo_Project_Installer/ReadMe.txt
@@ -0,0 +1,3 @@
+This project is an installer for the empty demo project. It shows how to add the custom action to the installer.
+
+See the readme.txt in the project 'xPLCheckPackage' for detailed instructions
---
+++ tieske.xPL_installation_aid/tags/version_1_0/xPLCheckPackage/MainCode.vb
@@ -0,0 +1,33 @@
+﻿Option Strict On
+
+Module MainCode
+    ''' <summary>
+    ''' This code is a container only, unpack resources included in temp folder and start them, exit immediately
+    ''' </summary>
+    ''' <remarks>Application must be started with parent ProcessID on the commandline</remarks>
+    Public Sub Main()
+        ' start up
+        Dim pid As Integer
+        If My.Application.CommandLineArgs.Count = 0 Then
+            pid = 0
+        Else
+            ' commandline argument, should be proces ID of installer
+            Try
+                ' get process ID from argument list
+                pid = CInt(My.Application.CommandLineArgs(0))
+
+            Catch ex As Exception
+                pid = 0
+            End Try
+        End If
+
+        ' We have started and have the parent (installer application) proces ID
+        ' Unpack resources to temp dir
+        Dim dir As String = Resources.tempPath
+        Resources.SaveResourceToFile(My.Resources.Interop_NetFwTypeLib, dir & "Interop.NetFwTypeLib.dll")
+        Resources.SaveResourceToFile(My.Resources.xpllib, dir & "xpllib.dll")
+        Resources.SaveResourceToFile(My.Resources.xPL_Hub_Verifier, dir & "xPL Hub Verifier.exe")
+        Process.Start(dir & "xPL Hub Verifier.exe", pid.ToString)
+        ' unpacked files, process started, we're done, exit
+    End Sub
+End Module
