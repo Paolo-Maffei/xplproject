2111297f7aec8a6bfd6b47ae865dc4e0df410564
timdawe99 <timdawe99@f80ddf92-7da3-a8d7-59b9-a95cd208da60>
2010-03-29 20:09:32 +0000

    Initial commit of xPLCurrentCost release 0.1

---
+++ slateblu.xPLCurrentCost/trunk/xplCurrentCost/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("xplCurrentCost")> 
+<Assembly: AssemblyDescription("xPL gateway for Curront Cost electricity monitoring device")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("xplCurrentCost")> 
+<Assembly: AssemblyCopyright("Copyright ©  2008 Tim Dawe")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("02cd4717-6016-4c84-8f76-b85895ce7a6c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.1.0.0")> 
+<Assembly: AssemblyFileVersion("0.1.0.0")> 
