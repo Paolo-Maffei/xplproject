13210a0541721ca024d3f685a62ced934b80f437
thijs@thijsschreijer.nl <thijs@thijsschreijer.nl@f80ddf92-7da3-a8d7-59b9-a95cd208da60>
2011-04-27 08:39:20 +0000

    update text files

--- tieske.xPL_WireShark_dissector/xpl_dissector.lua
+++ tieske.xPL_WireShark_dissector/xpl_dissector.lua
@@ -1,4 +1,39 @@
--- xPL protocol
+--[[
+WireShark dissector for xPL homeautomation protocol
+===================================================
+Copyright 2011 by Thijs Schreijer
+thijs@thijsschreijer.nl
+http://www.thijsschreijer.nl
+
+Feedback is very welcome.
+
+License
+=======
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Links
+=====
+WireShark       http://www.wireshark.org/
+xPL protocol    http://xplproject.org.uk/
+
+
+Changelog
+=========
+26-apr-2011   v1.0     Initial version
+
+]]--
 
 -- declare our protocol
 local xpl_proto = Proto("xPL","xPL Protocol")
