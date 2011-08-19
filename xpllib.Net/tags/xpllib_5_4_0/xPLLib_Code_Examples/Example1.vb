1d90d98f4dc183f5d27de738d5e3e19c745fab1c
thijs@thijsschreijer.nl <thijs@thijsschreijer.nl@f80ddf92-7da3-a8d7-59b9-a95cd208da60>
2011-08-19 16:09:39 +0000

    Released version 5.4.

---
+++ xpllib.Net/tags/xpllib_5_4_0/xPLLib_Code_Examples/Example1.vb
@@ -0,0 +1,43 @@
+﻿Imports xPL             ' import xPL namespace for quick access
+Imports xPL.xPL_Base    ' import the base to, provides numerous constants and support functions
+
+Module Example1
+    Dim dev1 As xPLDevice
+    Dim dev2 As xPLDevice
+    Dim dev3 As xPLDevice
+
+    Friend Sub Example1()
+        dev1 = New xPLDevice
+        dev2 = New xPLDevice
+        dev3 = New xPLDevice
+
+        MsgBox("We'll be creating some simple xPL devices, with different names, please start your xPL logger")
+        ' don't set anything, just enable it to go online
+        dev1.Enable()
+        MsgBox("First device was created using a randomized InstanceID, this is the default setting." & vbCrLf & _
+               "xPL address: " & dev1.Address & vbCrLf & vbCrLf & "The following device will get a specifically set Vendor and Device ID, and a Instance ID based on the host computers name.")
+        ' the second one, set the ID's and make it hostname based
+        dev2.VendorID = "tieske"
+        dev2.DeviceID = "example1"
+        dev2.InstanceIDType = InstanceCreation.HostNameBased
+        dev2.Enable()
+        MsgBox("Check your logger, you now have added: " & dev2.Address & vbCrLf & vbCrLf & "The next one will show how to create a device with a specifically set InstanceID")
+        ' this time also set the instance id
+        dev3.VendorID = "tieske"
+        dev3.DeviceID = "example1"
+        dev3.InstanceID = "totallymine"
+        dev3.Enable()
+        MsgBox("The last one (" & dev3.Address & ") has been created. The example has finished, click OK to clean up.")
+
+        ' cleanup, disposing will sent a proper end message
+        dev1.Dispose()
+        dev2.Dispose()
+        dev3.Dispose()
+
+        dev1 = Nothing
+        dev2 = Nothing
+        dev3 = Nothing
+
+        MsgBox("Devices have been cleaned up.")
+    End Sub
+End Module
---
+++ xpllib.Net/tags/xpllib_5_4_0/xpllib/ReadMe_Developer.txt
@@ -0,0 +1,30 @@
+How to create a new release;
+
+1 - Update version numbering in code
+      - check the assembly info on the project tab (the build number is going to be the build number
+        shown +1 if auto increment is used)
+      - update all code; comments at the top include version info to be updated
+      - update xPL_Base module: constant XPL_LIB_VERSION
+
+2 - Verify that the following methods;
+      - xPLDevice.New(ByVal SavedState As String, ByVal RestoreEnabled As Boolean)
+      - xPLListener.RestoreFromState(ByVal SavedState As String, ByVal RestoreEnabled As Boolean)
+    are capable of parsing any changes in the State strings, as they include version info.
+    
+2 - update the readme file, with changelog for buildnumber xxx
+
+3 - set type to "release" and Rebuild solution
+
+4 - exit Visual Studio, saving any changes
+
+5 - package the following files from the bin/release folder
+       xpllib.dll        : the actual library
+       xpllib.xml        : documentation file
+       gpl.txt           : license document
+       readme.txt        : readme documentation
+
+6 - publish the package on the website
+
+7 - update the xml plugin file with the updated versions
+
+
