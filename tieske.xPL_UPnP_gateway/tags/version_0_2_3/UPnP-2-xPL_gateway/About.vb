be3aa56ae6dad43121c2e14e43042786c6bd46cf
thijs@thijsschreijer.nl <thijs@thijsschreijer.nl@f80ddf92-7da3-a8d7-59b9-a95cd208da60>
2011-12-28 09:16:56 +0000

    released version 0.2.3, tagged

---
+++ tieske.xPL_UPnP_gateway/tags/version_0_2_3/UPnP-2-xPL_gateway/About.vb
@@ -0,0 +1,43 @@
+﻿Public NotInheritable Class About
+
+    Private Sub About_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Set the title of the form.
+        Dim ApplicationTitle As String
+        If My.Application.Info.Title <> "" Then
+            ApplicationTitle = My.Application.Info.Title
+        Else
+            ApplicationTitle = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+        End If
+        Me.Text = String.Format("About {0}", ApplicationTitle)
+        ' Initialize all of the text displayed on the About Box.
+        Me.LabelProductName.Text = My.Application.Info.ProductName
+        Me.LabelVersion.Text = String.Format("Version {0}", My.Application.Info.Version.ToString)
+        Me.LabelCopyright.Text = My.Application.Info.Copyright
+        Me.LabelCompanyName.Text = My.Application.Info.CompanyName
+        Dim readme As String
+        Try
+            readme = System.IO.File.ReadAllText(My.Application.Info.DirectoryPath & "\ReadMe.txt")
+        Catch ex As Exception
+            readme = "Unable to load " & My.Application.Info.DirectoryPath & "\ReadMe.txt"
+        End Try
+        Me.TextBoxDescription.Text = My.Application.Info.Description & vbCrLf & vbCrLf & readme
+        Me.LogoPictureBox.Image = xPL.xPL_Base.XPL_Icon.ToBitmap
+        Me.Icon = xPL.xPL_Base.XPL_Icon
+    End Sub
+
+    Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
+        Me.Close()
+    End Sub
+
+    'Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.Click
+    'End Sub
+
+    Private Sub LogoPictureBox_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LogoPictureBox.Click
+        System.Diagnostics.Process.Start("http://xplproject.org.uk/")
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        LinkLabel1.LinkVisited = True
+        System.Diagnostics.Process.Start("http://www.thijsschreijer.nl")
+    End Sub
+End Class
