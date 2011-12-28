5ca05ce23a7c8eb4320182c27a69540404530c41
thijs@thijsschreijer.nl <thijs@thijsschreijer.nl@f80ddf92-7da3-a8d7-59b9-a95cd208da60>
2011-12-28 09:16:56 +0000

    released version 0.2.3, tagged

---
+++ tieske.xPL_UPnP_gateway/tags/version_0_2_3/UPnP-2-xPL_gateway/MainForm.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.lbLog = New System.Windows.Forms.ListBox
+        Me.tbLogLines = New System.Windows.Forms.TextBox
+        Me.lblLinesToKeep = New System.Windows.Forms.Label
+        Me.btnCopyToClipboard = New System.Windows.Forms.Button
+        Me.TaskBarIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.PopupMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ShowLogToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.btnClose = New System.Windows.Forms.Button
+        Me.chkStartMinimized = New System.Windows.Forms.CheckBox
+        Me.btnClear = New System.Windows.Forms.Button
+        Me.PopupMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lbLog
+        '
+        Me.lbLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lbLog.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbLog.FormattingEnabled = True
+        Me.lbLog.ItemHeight = 14
+        Me.lbLog.Location = New System.Drawing.Point(12, 12)
+        Me.lbLog.Name = "lbLog"
+        Me.lbLog.Size = New System.Drawing.Size(721, 256)
+        Me.lbLog.TabIndex = 2
+        '
+        'tbLogLines
+        '
+        Me.tbLogLines.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.tbLogLines.Location = New System.Drawing.Point(213, 280)
+        Me.tbLogLines.Name = "tbLogLines"
+        Me.tbLogLines.Size = New System.Drawing.Size(61, 20)
+        Me.tbLogLines.TabIndex = 4
+        '
+        'lblLinesToKeep
+        '
+        Me.lblLinesToKeep.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblLinesToKeep.AutoSize = True
+        Me.lblLinesToKeep.Location = New System.Drawing.Point(136, 283)
+        Me.lblLinesToKeep.Name = "lblLinesToKeep"
+        Me.lblLinesToKeep.Size = New System.Drawing.Size(71, 13)
+        Me.lblLinesToKeep.TabIndex = 5
+        Me.lblLinesToKeep.Text = "Lines to keep"
+        '
+        'btnCopyToClipboard
+        '
+        Me.btnCopyToClipboard.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnCopyToClipboard.Location = New System.Drawing.Point(12, 278)
+        Me.btnCopyToClipboard.Name = "btnCopyToClipboard"
+        Me.btnCopyToClipboard.Size = New System.Drawing.Size(48, 23)
+        Me.btnCopyToClipboard.TabIndex = 6
+        Me.btnCopyToClipboard.Text = "Copy"
+        Me.btnCopyToClipboard.UseVisualStyleBackColor = True
+        '
+        'TaskBarIcon
+        '
+        Me.TaskBarIcon.BalloonTipText = "UPnP-2-xPL gateway"
+        Me.TaskBarIcon.ContextMenuStrip = Me.PopupMenu
+        Me.TaskBarIcon.Icon = CType(resources.GetObject("TaskBarIcon.Icon"), System.Drawing.Icon)
+        Me.TaskBarIcon.Visible = True
+        '
+        'PopupMenu
+        '
+        Me.PopupMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem, Me.ShowLogToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.PopupMenu.Name = "PopupMenu"
+        Me.PopupMenu.Size = New System.Drawing.Size(141, 70)
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.AboutToolStripMenuItem.Text = "About..."
+        '
+        'ShowLogToolStripMenuItem
+        '
+        Me.ShowLogToolStripMenuItem.Name = "ShowLogToolStripMenuItem"
+        Me.ShowLogToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.ShowLogToolStripMenuItem.Text = "Show log..."
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'btnClose
+        '
+        Me.btnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnClose.Location = New System.Drawing.Point(658, 278)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 7
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'chkStartMinimized
+        '
+        Me.chkStartMinimized.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.chkStartMinimized.AutoSize = True
+        Me.chkStartMinimized.Checked = Global.UPnP2xPL.My.MySettings.Default.StartMinimized
+        Me.chkStartMinimized.DataBindings.Add(New System.Windows.Forms.Binding("Checked", Global.UPnP2xPL.My.MySettings.Default, "StartMinimized", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.chkStartMinimized.Location = New System.Drawing.Point(294, 282)
+        Me.chkStartMinimized.Name = "chkStartMinimized"
+        Me.chkStartMinimized.Size = New System.Drawing.Size(96, 17)
+        Me.chkStartMinimized.TabIndex = 3
+        Me.chkStartMinimized.Text = "Start minimized"
+        Me.chkStartMinimized.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnClear.Location = New System.Drawing.Point(66, 278)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(48, 23)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(745, 302)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnCopyToClipboard)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.lblLinesToKeep)
+        Me.Controls.Add(Me.lbLog)
+        Me.Controls.Add(Me.tbLogLines)
+        Me.Controls.Add(Me.chkStartMinimized)
+        Me.MinimumSize = New System.Drawing.Size(539, 163)
+        Me.Name = "MainForm"
+        Me.Text = "UPnP 2 xPL gateway"
+        Me.PopupMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbLog As System.Windows.Forms.ListBox
+    Friend WithEvents chkStartMinimized As System.Windows.Forms.CheckBox
+    Friend WithEvents tbLogLines As System.Windows.Forms.TextBox
+    Friend WithEvents lblLinesToKeep As System.Windows.Forms.Label
+    Friend WithEvents btnCopyToClipboard As System.Windows.Forms.Button
+    Friend WithEvents TaskBarIcon As System.Windows.Forms.NotifyIcon
+    Friend WithEvents PopupMenu As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ShowLogToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ tieske.xPL_UPnP_gateway/tags/version_0_2_3/UPnP-2-xPL_gateway/MainForm.vb
@@ -0,0 +1,173 @@
+﻿Imports OpenSource.UPnP
+Imports xPL
+Imports xPL.xPL_Base
+Imports System.Xml
+
+Public Class MainForm
+
+    Private WithEvents cp As UPnPSmartControlPoint
+    Private CacheTime As Integer = 900
+    Private xset As String
+
+#Region "Form startup and shutdown"
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' Load xPL devices first, do not enable them
+        xPLListener.RestoreFromState(My.Settings.xPLDevices, False)
+        ' cleanup just to be sure
+        Select Case xPLListener.Count
+            Case 0
+                ' Need to create a new one
+                Dim dev As New xPLDevice
+                dev.VendorID = "tieske"
+                dev.DeviceID = "upnp"
+                dev.InstanceIDType = InstanceCreation.HostNameBased
+                dev.Configurable = False
+                dev.MessagePassing = dev.MessagePassing Or MessagePassingEnum.PassWhileAwaitingConfig
+                LogMessage("New xPLDevice created; " & dev.Address & " (status: " & CStr(IIf(dev.Configured, "configured", "unconfigured")) & ")")
+            Case 1
+                ' This is ok.
+                LogMessage("Existing xPLDevice restarted; " & xPLListener.Device(0).Address & " (status: " & CStr(IIf(xPLListener.Device(0).Configured, "configured", "unconfigured")) & ")")
+            Case Is > 1
+                ' delete the too many devices
+                For n As Integer = xPLListener.Count - 1 To 1 Step -1
+                    xPLListener.Device(n).Dispose()
+                Next
+                LogMessage("Existing xPLDevice restarted; " & xPLListener.Device(0).Address & " (status: " & CStr(IIf(xPLListener.Device(0).Configured, "configured", "unconfigured")) & ")")
+        End Select
+        Proxy.xPLDevice = xPLListener.Device(0)
+        Proxy.xPLDevice.Enable()
+        ' Now enable UPnP control point, devices will be found
+        cp = New UPnPSmartControlPoint
+        ' configure form
+        Me.Icon = XPL_Icon
+        LogMessage("UPnP-2-xPL gateway started")
+        ' read settings
+        Me.tbLogLines.Text = My.Settings.LogLines.ToString
+        If My.Settings.StartMinimized Then
+            Me.WindowState = FormWindowState.Minimized
+            Me.ShowInTaskbar = False
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        ' make sure we properly stop everything
+        ' store current xPL devices and stop xPL devices
+        My.Settings.xPLDevices = xPLListener.GetState(xPL_Base.GetVersionNumber(2))
+        xPLListener.Shutdown()
+        ' stop UPnP control point and dispose
+        cp = Nothing
+    End Sub
+
+#End Region
+
+#Region "Logger functionality"
+
+    Private LogLock As New Object
+    Private Sub lmgo(ByVal message As String)
+        SyncLock LogLock
+            ' replace control chars to proper line separators
+            message = message.Replace(vbCrLf, Chr(0))
+            message = message.Replace(vbLf, Chr(0))
+            message = message.Replace(vbCr, Chr(0))
+            ' make an array of lines
+            Dim s() As String = message.Split(Chr(0))
+            Dim i As Integer
+            ' add lines to the log
+            Dim t As Date = Now()
+            Dim ts As String = t.ToString("HH:mm:ss.fff")
+            For Each message In s
+                i = lbLog.Items.Add(ts & " " & message)
+                lbLog.SelectedIndex = i
+                ' limit list to configured number of items
+                While lbLog.Items.Count > My.Settings.LogLines
+                    lbLog.Items.RemoveAt(0)
+                End While
+            Next
+        End SyncLock
+    End Sub
+    Private Delegate Sub lmgod(ByVal message As String)
+    Private lmgods As lmgod = AddressOf lmgo
+    Private Sub LogMessage(ByVal message As String)
+        If lbLog.InvokeRequired Then
+            Me.Invoke(lmgods, message)
+        Else
+            lmgo(message)
+        End If
+    End Sub
+#End Region
+
+#Region "Device handling"
+
+    Private Sub AddDevice(ByVal sender As OpenSource.UPnP.UPnPSmartControlPoint, _
+                      ByVal device As OpenSource.UPnP.UPnPDevice _
+                      ) Handles cp.OnAddedDevice
+        Proxy.AddDevice(device, AddressOf LogMessage)
+    End Sub
+
+    Private Sub RemDevice(ByVal sender As OpenSource.UPnP.UPnPSmartControlPoint, _
+                      ByVal device As OpenSource.UPnP.UPnPDevice _
+                      ) Handles cp.OnRemovedDevice
+        Proxy.RemoveDevice(device)
+    End Sub
+#End Region
+
+#Region "Interface elements"
+
+    Private Sub chkStartMinimized_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkStartMinimized.CheckedChanged
+        My.Settings.StartMinimized = chkStartMinimized.Checked
+    End Sub
+
+    Private Sub btnCopyToClipboard_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCopyToClipboard.Click
+        Dim sb As New System.Text.StringBuilder
+        For Each s As String In lbLog.Items
+            sb.Append(s & vbCrLf)
+        Next
+        Clipboard.SetText(sb.ToString, TextDataFormat.UnicodeText)
+    End Sub
+
+    Private Sub ShowLogToolStripMenuItem_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles ShowLogToolStripMenuItem.Click
+        Me.ShowInTaskbar = True
+        Me.WindowState = FormWindowState.Normal
+        Me.Visible = True
+    End Sub
+
+    Private Sub btnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClose.Click
+        Me.ShowInTaskbar = False
+        Me.Visible = False
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TaskBarIcon_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles TaskBarIcon.DoubleClick
+        Me.ShowLogToolStripMenuItem_Click(Nothing, Nothing)
+    End Sub
+
+    Dim frmAbout As About
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        If frmAbout Is Nothing Then
+            frmAbout = New About
+            frmAbout.ShowDialog()
+            frmAbout = Nothing
+        Else
+            frmAbout.Show()
+        End If
+    End Sub
+
+    Private Sub tbLogLines_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles tbLogLines.LostFocus
+        Dim l As Long = Int(Val(tbLogLines.Text))
+        If l < 20 Then l = 20
+        If l > 20000 Then l = 20000
+        tbLogLines.Text = l.ToString
+        My.Settings.LogLines = l
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        lbLog.Items.Clear()
+    End Sub
+
+#End Region
+
+End Class
